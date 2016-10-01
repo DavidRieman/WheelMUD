003cc88091548a6a78a18509fa6d3cfb92a74d79
David Rieman <davidrieman@users.noreply.github.com>
2016-10-01 12:15:52 -0700

    Fix a couple disposables sitting around until GC'd. * Also fix a few StyleCop compliance warnings while in there. * Testing Travis CI with this change.

--- src/Main/Application.cs
+++ src/Main/Application.cs
@@ -134,11 +134,12 @@ namespace WheelMUD.Main
                     sourcePath = Path.GetDirectoryName(appDir);
                     sourcePath = Path.Combine(sourcePath + "\\systemdata\\Files\\");
                 }
-                DirectoryCopy(sourcePath, destDir,true);
+
+                DirectoryCopy(sourcePath, destDir, true);
             }
         }
-        private static void DirectoryCopy(
-        string sourceDirName, string destDirName, bool copySubDirs)
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
         {
             DirectoryInfo dir = new DirectoryInfo(sourceDirName);
             DirectoryInfo[] dirs = dir.GetDirectories();
@@ -156,8 +157,7 @@ namespace WheelMUD.Main
             {
                 Directory.CreateDirectory(destDirName);
             }
-
-
+            
             // Get the file contents of the directory to copy.
             FileInfo[] files = dir.GetFiles();
 
@@ -173,7 +173,6 @@ namespace WheelMUD.Main
             // If copySubDirs is true, copy the subdirectories.
             if (copySubDirs)
             {
-
                 foreach (DirectoryInfo subdir in dirs)
                 {
                     // Create the subdirectory.
@@ -183,7 +182,6 @@ namespace WheelMUD.Main
                     DirectoryCopy(subdir.FullName, temppath, copySubDirs);
                 }
             }
-
         }
 
         /// <summary>Stop the application.</summary>
@@ -210,11 +208,9 @@ namespace WheelMUD.Main
         /// </summary>
         public void DisplayHelp()
         {
-            string path = Configuration.GetDataStoragePath();
-
-            var sr = new StreamReader(Path.Combine(path, "ConsoleHelp.txt"));
-            string splash = sr.ReadToEnd();
-            this.Notify(this.viewEngine.RenderView(splash));
+            var path = Path.Combine(Configuration.GetDataStoragePath(), "ConsoleHelp.txt");
+            var help = File.ReadAllText(path);
+            this.Notify(this.viewEngine.RenderView(help));
         }
 
         /// <summary>
@@ -280,9 +276,7 @@ namespace WheelMUD.Main
                 }
             }
         }
-
-
-
+        
         /// <summary>
         /// Initializes the systems of this application.
         /// </summary>
@@ -338,14 +332,12 @@ namespace WheelMUD.Main
             return systems;
         }
 
-        /// <summary>
-        /// Display startup texts.
-        /// </summary>
+        /// <summary>Display startup texts.</summary>
         /// <returns>The startup splash screen text.</returns>
         private string DisplayStartup()
         {
-            var sr = new StreamReader(Path.Combine(Configuration.GetDataStoragePath(), "ConsoleOpen.txt"));
-            string splash = sr.ReadToEnd();
+            var filePath = Path.Combine(Configuration.GetDataStoragePath(), "ConsoleOpen.txt");
+            var splash = File.ReadAllText(filePath);
             return this.viewEngine.RenderView(splash);
         }
     }
