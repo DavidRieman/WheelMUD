8ce941430e2b12bf3141a00640f54fdbb96f2003
David Rieman <davidrieman@users.noreply.github.com>
2016-10-01 21:56:02 -0700

    Rebuilt the UnhandledExceptionHandler to be easier to use. * No longer have to keep an instance of a class around to utilize it; this is   required to fix an unused value warning-as-error in CI anyway.

--- src/Core/Exceptions/UnhandledExceptionHandler.cs
+++ src/Core/Exceptions/UnhandledExceptionHandler.cs
@@ -10,26 +10,34 @@ namespace WheelMUD.Core
     using System;
     using System.Diagnostics;
 
-    /// <summary>
-    /// A fairly generic unhandled exception handler.
-    /// </summary>
+    /// <summary>A fairly generic unhandled exception handler.</summary>
     public class UnhandledExceptionHandler
     {
-        private Action<string> notifier;
+        /// <summary>The active exception notifier.</summary>
+        private static Action<string> notifier;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="UnhandledExceptionHandler"/> class.
-        /// </summary>
-        /// <param name="notifier">An action that handles exception message strings in some way, e.g. print to the console.</param>
-        public UnhandledExceptionHandler(Action<string> notifier)
+        /// <summary>Registers a notifier for receiving unhandled exception details.</summary>
+        /// <remarks>
+        /// Once called, the UnhandledExceptionHandler tries not to let unhandled exceptions crash the program.
+        /// If the debugger is attached, the exception will be re-thrown to let the debugger handle it.  Otherwise,
+        /// unhandled exceptions will get logged to the assigned notifier (such as a printer to the console output
+        /// if running the server in console mode, or perhaps an event log during service mode, and so on).
+        /// </remarks>
+        /// <param name="newNotifier">An action that handles exception message strings in some way, e.g. print to the console.</param>
+        public static void Register(Action<string> newNotifier)
         {
-            this.notifier = notifier;
+            notifier = newNotifier;
 
-            // Set up the global unhandled exception handler for this app domain.
-            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(this.CurrentDomain_UnhandledException);
+            // Set up the global unhandled exception handler for this app domain, or re-register it without 
+            // doubling up, in the case of Register being called a second time (IE with a new notifier).
+            AppDomain.CurrentDomain.UnhandledException -= CurrentDomain_UnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
         }
 
-        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        /// <summary>Unhandled exception handler for the current domain.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The unhandled exception event arguments.</param>
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             // If the debugger is attached, rethrow the exception so the debugger can assess the issue
             // at the call site with the appropriate info right there.
@@ -39,7 +47,7 @@ namespace WheelMUD.Core
                 throw ex;
             }
 
-            this.notifier(ex.ToDeepString());
+            notifier(ex.ToDeepString());
         }
     }
 }
