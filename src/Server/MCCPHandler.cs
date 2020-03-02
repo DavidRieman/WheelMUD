fe13fbc587c5c038bb2dde3d3f46889f0c370403
David Rieman <davidrieman@users.noreply.github.com>
2020-03-02 00:02:49 -0800

    Repair MCCP handler (was crashing when connected with advanced telnet clients), and a few minor adjustments.

--- src/Server/MCCPHandler.cs
+++ src/Server/MCCPHandler.cs
@@ -3,18 +3,15 @@
 //   Copyright (c) WheelMUD Development Team.  See LICENSE.txt.  This file is 
 //   subject to the Microsoft Public License.  All other rights reserved.
 // </copyright>
-// <summary>
-//   Compresses our data into a byte array using zlib compression
-// </summary>
 //-----------------------------------------------------------------------------
 
 namespace WheelMUD.Server
 {
     using System.IO;
-    using System.Text;
     using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 
     /// <summary>The MUD Client Compression Protocol (MCCP) handler.</summary>
+    /// <remarks>Compresses our data into a byte array using zlib compression.</remarks>
     internal static class MCCPHandler
     {
         /// <summary>Compresses data using zlib.</summary>
@@ -22,24 +19,17 @@ namespace WheelMUD.Server
         /// <returns>A byte array containing the compressed data</returns>
         public static byte[] Compress(string data)
         {
-            // Commented out below as it loses extended ASCII (127+) and replaces with a ?
-            // byte[] bytes = ASCIIEncoding.ASCII.GetBytes(data);
-
-            // Encoding using default terminal extended ascii codepage 437
-            byte[] bytes = Encoding.GetEncoding(437).GetBytes(data);
+            byte[] bytes = Connection.CurrentEncoding.GetBytes(data);
             byte[] returnBytes;
-            using (var stream = new MemoryStream())
-            {
-                using (var compressedStream = new DeflaterOutputStream(stream))
-                {
-                    compressedStream.Write(bytes, 0, bytes.Length);
-                    compressedStream.Finish();
-                    stream.Position = 0;
+            
+            using var stream = new MemoryStream();
+            using var compressedStream = new DeflaterOutputStream(stream);
+            compressedStream.Write(bytes, 0, bytes.Length);
+            compressedStream.Finish();
+            stream.Position = 0;
 
-                    returnBytes = new byte[stream.Length];
-                    stream.Read(returnBytes, 0, returnBytes.Length);
-                }
-            }
+            returnBytes = new byte[stream.Length];
+            stream.Read(returnBytes, 0, returnBytes.Length);
 
             return returnBytes;
         }
