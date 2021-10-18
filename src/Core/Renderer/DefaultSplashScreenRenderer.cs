c79a9b025f9df3528d2566cf1e29f57ea6e5efb1
David Rieman <DavidRieman@users.noreply.github.com>
2021-10-18 00:03:28 -0700

    Fixed splash screen alignments for improved first impressions, and cleaned up the file itself.

--- src/Core/Renderer/DefaultSplashScreenRenderer.cs
+++ src/Core/Renderer/DefaultSplashScreenRenderer.cs
@@ -20,68 +20,63 @@ namespace WheelMUD.Core
         }
 
         private static string Get(int number = -1)
-                {
-                    var random = new Random();
-        
-                    if (number < 0 || number > SplashScreens.Length - 1)
-                    {
-                        return SplashScreens[random.Next(0, SplashScreens.Length)];
-                    }
-        
-                    return SplashScreens[number];
-                }
-        
-                private static readonly string[] SplashScreens =
-                {"<%green%>" + "<%nl%>" +
-                 " _       _  _                    __    __   __   _     _  _____  " + "<%nl%>" +
-                 "(_)  _  (_)(_)      ____   ____ (__)  (__)_(__) (_)   (_)(_____) " + "<%nl%>" +
-                 "(_) (_) (_)(_)__   (____) (____) (_) (_) (_) (_)(_)   (_)(_)  (_)" + "<%nl%>" +
-                 "(_) (_) (_)(____) (_)_(_)(_)_(_) (_) (_) (_) (_)(_)   (_)(_)  (_)" + "<%nl%>" +
-                 "(_)_(_)_(_)(_) (_)(__)__ (__)__  (_) (_)     (_)(_)___(_)(_)__(_)" + "<%nl%>" +
-                 "(__) (__) (_) (_) (____) (____)(___)(_)     (_) (_____) (_____)  " + "<%nl%>" +
-                 "<%n%>" + "<%nl%>" +
-                 $"{GameConfiguration.Copyright}" + "<%nl%>" +
-                 "" + "<%nl%>" +
-                 $"Welcome to {GameConfiguration.Name} {GameConfiguration.Version}.",
-                 
-                 "<%green%>" + "<%nl%>" +
-                 "Y8b Y8b Y888P 888                     888     e   e     8888 8888 888 88e" + "<%nl%>" +
-                 "Y8b Y8b Y8P  888 ee   ,e e,   ,e e,  888    d8b d8b    8888 8888 888 888b" + "<%nl%>" +
-                 "Y8b Y8b Y   888 88b d88 88b d88 88b 888   e Y8b Y8b   8888 8888 888 8888D" + "<%nl%>" +
-                 "Y8b Y8b    888 888 888   , 888   , 888  d8b Y8b Y8b  8888 8888 888 888P  " + "<%nl%>" +
-                 "Y8P Y     888 888  'YeeP'  'YeeP' 888 d888b Y8b Y8b 'Y88 88P' 888 88     " + "<%nl%>" +
-                 "<%n%>" + "<%nl%>" +
-                 $"{GameConfiguration.Copyright}" + "<%nl%>" +
-                 "" + "<%nl%>" +
-                 $"Welcome to {GameConfiguration.Name} {GameConfiguration.Version}.",
-                 
-                    "<%green%>" + "<%nl%>" +
-                    "       @@@@@@      @@@@@@@@                                       " + "<%nl%>" +
-                    "      @@    @@@  @@@      @@                                      " + "<%nl%>" +
-                    "      @        @@@@         @    @   @   @  @                    @" + "<%nl%>" +
-                    "     @@         @@          @@   @  @ @  @  @                    @" + "<%nl%>" +
-                    "    @@          @@           @   @  @ @  @  @ @@    @@@    @@@   @" + "<%nl%>" +
-                    "    @@          @@           @   @  @ @  @  @@  @  @   @  @   @  @" + "<%nl%>" +
-                    "    @@         @@@@@         @   @ @   @ @  @   @  @   @  @   @  @" + "<%nl%>" +
-                    "    @@      @@@@@@@@@@@     @@   @ @   @ @  @   @  @@@@@  @@@@@  @" + "<%nl%>" +
-                    "    @@     @@@@@@@@@@@@     @@   @ @   @ @  @   @  @      @      @" + "<%nl%>" +
-                    "    @@@    @@@@@@@@@@@@@   @@     @     @   @   @  @   @  @   @  @" + "<%nl%>" +
-                    "     @@   @@@@@@@@@@@@@@@ @@      @     @   @   @   @@@    @@@   @" + "<%nl%>" +
-                    "      @@@ @@@ @  @  @ @@@@@                                       " + "<%nl%>" +
-                    "        @@@@@  @ @ @  @@@@             @     @   @     @  @@@@@   " + "<%nl%>" +
-                    "         @@@    @@@    @@@             @@   @@   @     @  @    @  " + "<%nl%>" +
-                    "         @@@@ @ @@@ @ @@@@             @@   @@   @     @  @     @ " + "<%nl%>" +
-                    "         @@@    @@@    @@@             @ @ @ @   @     @  @     @ " + "<%nl%>" +
-                    "         @@@   @ @ @   @@@             @ @ @ @   @     @  @     @ " + "<%nl%>" +
-                    "          @@@ @  @  @ @@@              @ @ @ @   @     @  @     @ " + "<%nl%>" +
-                    "          @@@@   @   @@@@              @ @ @ @   @     @  @     @ " + "<%nl%>" +
-                    "           @@@@@@@@@@@@@@              @  @  @    @   @   @    @  " + "<%nl%>" +
-                    "            @@@@@@@@@@@                @  @  @     @@@    @@@@@   " + "<%nl%>" +
-                    "              @@@@@@                                              " + "<%nl%>" +
-                    "<%n%>" + "<%nl%>" +
-                    $"{GameConfiguration.Copyright}" + "<%nl%>" +
-                    "" + "<%nl%>" +
-                    $"Welcome to {GameConfiguration.Name} {GameConfiguration.Version}."
-                };
+        {
+            var random = new Random();
+
+            if (number < 0 || number > SplashScreens.Length - 1)
+            {
+                return SplashScreens[random.Next(0, SplashScreens.Length)];
+            }
+
+            return SplashScreens[number];
+        }
+
+        private static readonly string[] SplashScreens =
+        {
+            "<%green%><%nl%>" +
+            " _       _  _                    __    __   __   _     _  _____  <%nl%>" +
+            "(_)  _  (_)(_)      ____   ____ (__)  (__)_(__) (_)   (_)(_____) <%nl%>" +
+            "(_) (_) (_)(_)__   (____) (____) (_) (_) (_) (_)(_)   (_)(_)  (_)<%nl%>" +
+            "(_) (_) (_)(____) (_)_(_)(_)_(_) (_) (_) (_) (_)(_)   (_)(_)  (_)<%nl%>" +
+            "(_)_(_)_(_)(_) (_)(__)__ (__)__  (_) (_)     (_)(_)___(_)(_)__(_)<%nl%>" +
+            " (__) (__) (_) (_) (____) (____)(___)(_)     (_) (_____) (_____) <%nl%>" +
+            $"<%n%><%nl%>{GameConfiguration.Copyright}<%nl%><%nl%>" +
+            $"Welcome to {GameConfiguration.Name} {GameConfiguration.Version}.<%nl%>",
+
+            "<%green%><%nl%>" +
+            "Y8b Y8b Y888P 888                     888     e   e     8888 8888 888888e  <%nl%>" +
+            " Y8b Y8b Y8P  888eee   ,eee,   ,eee,  888    d8b d8b    8888 8888 888 888b <%nl%>" +
+            "  Y8b Y8b Y   888888b d88_d8) d88_d8) 888   e Y8b Y8b   8888 8888 888  888D<%nl%>" +
+            "   Y8b Y8b    888 888 888     888     888  d8b Y8b Y8b  888888888 888 d88P <%nl%>" +
+            "    Y8P Y     888 888 'Y8eeP' 'Y8eeP' 888 d888b Y8b Y8b 'Y88888P' 888888   <%nl%>" +
+            $"<%n%><%nl%>{GameConfiguration.Copyright}<%nl%><%nl%>" +
+            $"Welcome to {GameConfiguration.Name} {GameConfiguration.Version}.<%nl%>",
+
+            "<%green%><%nl%>" +
+            "   @@@@@@      @@@@@@@@   <%nl%>" +
+            "  @@    @@@  @@@      @@  <%nl%>" +
+            "  @        @@@@         @    @   @   @  @                    @<%nl%>" +
+            " @@         @@          @@   @  @ @  @  @                    @<%nl%>" +
+            "@@          @@           @   @  @ @  @  @ @@    @@@    @@@   @<%nl%>" +
+            "@@          @@           @   @  @ @  @  @@  @  @   @  @   @  @<%nl%>" +
+            "@@         @@@@@         @   @ @   @ @  @   @  @   @  @   @  @<%nl%>" +
+            "@@      @@@@@@@@@@@     @@   @ @   @ @  @   @  @@@@@  @@@@@  @<%nl%>" +
+            "@@     @@@@@@@@@@@@     @@   @ @   @ @  @   @  @      @      @<%nl%>" +
+            "@@@    @@@@@@@@@@@@@   @@     @     @   @   @  @   @  @   @  @<%nl%>" +
+            " @@   @@@@@@@@@@@@@@@ @@      @     @   @   @   @@@    @@@   @<%nl%>" +
+            "  @@@ @@@ @  @  @ @@@@@   <%nl%>" +
+            "    @@@@@  @ @ @  @@@@             @     @   @     @  @@@@@   <%nl%>" +
+            "     @@@    @@@    @@@             @@   @@   @     @  @    @  <%nl%>" +
+            "     @@@@ @ @@@ @ @@@@             @@   @@   @     @  @     @ <%nl%>" +
+            "     @@@    @@@    @@@             @ @ @ @   @     @  @     @ <%nl%>" +
+            "     @@@   @ @ @   @@@             @ @ @ @   @     @  @     @ <%nl%>" +
+            "      @@@ @  @  @ @@@              @ @ @ @   @     @  @     @ <%nl%>" +
+            "      @@@@   @   @@@@              @ @ @ @   @     @  @     @ <%nl%>" +
+            "       @@@@@@@@@@@@@@              @  @  @    @   @   @    @  <%nl%>" +
+            "        @@@@@@@@@@@                @  @  @     @@@    @@@@@   <%nl%>" +
+            "          @@@@@@          <%nl%>" +
+            $"<%n%><%nl%>{GameConfiguration.Copyright}<%nl%><%nl%>" +
+            $"Welcome to {GameConfiguration.Name} {GameConfiguration.Version}.<%nl%>",
+        };
     }
 }
