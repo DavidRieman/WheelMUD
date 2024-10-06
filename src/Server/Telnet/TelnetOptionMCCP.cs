318694d645188a11a03fc18e02bc1ac9b59829e8
David Rieman <DavidRieman@users.noreply.github.com>
2024-10-05 22:02:14 -0700

    Added notes to MCCP telnet option.

--- src/Server/Telnet/TelnetOptionMCCP.cs
+++ src/Server/Telnet/TelnetOptionMCCP.cs
@@ -7,7 +7,15 @@
 
 namespace WheelMUD.Server.Telnet
 {
-    /// <summary>Class that handles the subnegotiation of an MCCP (Compression) telnet option code.</summary>
+    /// <summary>Class that handles the subnegotiation of an MCCP2 (Compression) telnet option code.</summary>
+    /// <remarks>
+    /// Although MCCP version 1 was a separate option code (85), we intentionally do not support the original:
+    /// MCCP v1 was quickly deprecated due to an invalid subnegotiation sequence. Thus usually the only version
+    /// of MCCP one will want to support will be this version (MCCP2 via option code 86).
+    /// Note there may be suggestion of an MCCP3, but it is likely not well supported and generally may be
+    /// considered fully redundant as MCCP2 can technically already be bi-directional if a client wants that.
+    /// (The server does not support client-side compression yet, but it should be easy to add if found useful.)
+    /// </remarks>
     internal class TelnetOptionMCCP : TelnetOption
     {
         /// <summary>Initializes a new instance of the TelnetOptionMCCP class.</summary>
